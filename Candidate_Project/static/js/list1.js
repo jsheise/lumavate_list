d79e1121482d993f214e6edead7f2d29746cbfe6
jsheise <heisejoe@gmail.com>
2019-05-28 14:36:21 -0700

    basic angular framework now working with Flask, but lost proper data

--- Candidate_Project/static/js/list1.js
+++ Candidate_Project/static/js/list1.js
@@ -1,17 +1,23 @@
 var list1 = angular.module('list1', []);
 
+// have to configure angular to use {a a} bracket notation to avoid conflicts with Jinja engine
+app.config(['$interpolateProvider', function(interpolateProvider) {
+  $interpolateProvider.startSymbol('{a');
+  $interpolateProvider.startSymbol('a}');
+}]);
+
 list1.controller('listCtrl', function($scope) {
- 
+
   $scope.music = [
     {artist: "Bowling for Soup", song: "1985", album: "A Hangover You Don't Deserve", year: "2004", favorite: false, edit: false },
     {artist: "Periphery", song: "Muramasa", album: "Periphery II: This Time It's Personal", year: "2012", favorite: true, edit: false},
     {artist: "Soundgarden", song: "Black Hole Sun", album: "Superunknown", year: "1994", favorite: false, edit: false }
   ];
- 
+
   // Receives the new item entered in the input box and puts
   // it on the end of the array
   $scope.addEntry = function(newEntry) {
- 
+
     // Check that the input box has a value
     if(!(newEntry === undefined || newEntry === "")){
       $scope.music.push({
@@ -19,7 +25,7 @@ list1.controller('listCtrl', function($scope) {
       });
       $scope.missingNewItemError = "";
     } else {
- 
+
       // Show an error if no item was entered
       $scope.missingNewItemError = "Please Enter an Entry";
     }
