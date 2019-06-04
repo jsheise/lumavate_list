0679069c4147ff7bb1717cc76370c992cbeb6f81
jsheise <heisejoe@gmail.com>
2019-06-04 15:39:08 -0700

    finished project; however, no current usage of AngularJS in HTML

--- Candidate_Project/app.py
+++ Candidate_Project/app.py
@@ -1,26 +1,18 @@
 from flask import Flask, render_template, request, redirect
 # from flask_restful import Resource, Api
 from flask_triangle import Triangle
-
-from flask_heroku import Heroku #environment variables
+#from flask_heroku import Heroku #environment variables
 from flask_sqlalchemy import SQLAlchemy
-
 import sys, json # necessary?
 import os # os allows for retrieval of environment variables (done in test, but may not be necessary with flask_heroku)
-
-# taken from https://www.codementor.io/garethdwyer/building-a-crud-application-with-flask-and-sqlalchemy-dm3wv7yu2
-# (got an error with music_db cannot be found)
-#project_dir = os.path.dirname(os.path.abspath(__file__))
-#database_file = "postgresql:///{}".format(os.path.join(project_dir, "music_database.db"))
-
 
 app = Flask(__name__)
 Triangle(app) # allows for AngularJS expressions filter
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # TEMP - set database URI for local testing of database entries
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///music_db"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///music_db"
 app.config.from_object(os.environ['APP_SETTINGS'])
-heroku = Heroku(app) # environment variables taken care of and database config set up
+#heroku = Heroku(app) # environment variables taken care of and database config set up
 db = SQLAlchemy(app) # database object
 
 # CAN be put in separate file, but no need here
@@ -54,13 +46,11 @@ class MusicEntry(db.Model):
             'rating_0_5':self.rating_0_5
         }
 
+#db.create_all()
 # retrieve list of music entries from database
 musicList = MusicEntry.query.all() # need to add musicList to render_template later
 editItem = {}
-
 editId = 0
-
-tempBool = False
 
 @app.route('/', methods=["POST", "GET"])
 def home():
@@ -69,13 +59,12 @@ def home():
         db.session.add(newEntry)
         db.session.commit()
     musicList = MusicEntry.query.all()
-    return render_template('list.html', musicList=musicList, tempBool=tempBool) #, entryData = EntryData)
+    return render_template('list.html', musicList=musicList) #, entryData = EntryData)
 
 @app.route("/update", methods=["POST"])
 def update():
     editId = request.form.get("idNum")
     editSelection = MusicEntry.query.filter_by(id=editId).first()
-    tempBool = False
     # editSelection.song = newname
     # db.session.commit()
     return render_template('entryUpdate.html', editItem=editSelection)
@@ -105,34 +94,5 @@ def delete():
     db.session.commit()
     return redirect("/")
 
-# @app.route('/addEntry', methods=["POST"])
-# def addEntry():
-#     try:
-#         json_data = request.json['newEntry']
-#         songName = json_data[song]
-#         artistName = json_data[artist]
-#         albumName = json_data[album]
-#         yearNum = json_data[year]
-#         ratingNum = json_data[rating]
-#
-#         newMusicEntry = MusicEntry(song=songName, artist=artistName, album=albumName, year=yearNum, rating_0_5=ratingNum)
-#         db.session.add(newMusicEntry)
-#         db.session.commit()
-#         return render_template('list.html', musicList=musicList) #, entryData = EntryData)
-#     except Exception(e):
-#         return str(e)
-#     return 'added'
-
 if __name__ == '__main__':
     app.run()
-
-# BELOW IS USING flask_restful LIBRARY
-# EntryData = EntryData
-
-# api = Api(app)
-#
-# class ListItem(Resource):
-#     def get(self):
-#         return render_template('list.html');
-#
-# api.add_resource(ListItem, '/')
